e6b11b8973b37347652f286abde668f6f9b54af3
RojoGastronomia <suporte@rojogastronomia.com>
2025-05-02 15:09:52 -0300

    Ajustes de migração e configuração para deploy

--- migrations/003_add_venue_room_to_orders.sql
+++ migrations/003_add_venue_room_to_orders.sql
@@ -4,5 +4,5 @@ ALTER TABLE orders
 ADD COLUMN room_id INTEGER REFERENCES rooms(id);
 
 -- Create indexes for better performance
-CREATE INDEX idx_orders_venue_id ON orders(venue_id);
+-- CREATE INDEX idx_orders_venue_id ON orders(venue_id);
 CREATE INDEX idx_orders_room_id ON orders(room_id);
