9b5bde3f18ede7e632dfadb2b1388f3db88a3e08
RojoGastronomia <suporte@rojogastronomia.com>
2025-07-14 12:08:29 -0300

    Fix model imports to include .js extensions

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,9 +1,9 @@
 // Exportar todos os modelos MongoDB
-export { User, type IUser } from './User';
-export { Menu, type IMenu } from './Menu';
-export { Dish, type IDish } from './Dish';
-export { Event, type IEvent } from './Event';
-export { Order, type IOrder } from './Order';
+export { User, type IUser } from './User.js';
+export { Menu, type IMenu } from './Menu.js';
+export { Dish, type IDish } from './Dish.js';
+export { Event, type IEvent } from './Event.js';
+export { Order, type IOrder } from './Order.js';
 
 // Re-exportar mongoose para conveniência
 export { default as mongoose } from 'mongoose'; 
